52665bb35f39886602801bcbc4f7a75202f09b4c
amit <ashethe@deqode.com>
2021-12-20 17:39:56 +0530

    Carts controller,model and View updated

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,14 @@
 class CartsController < ApplicationController
-  def show
+  before_action :intialize_Cart_Object, only: %i[show destroy]
+
+  def destroy
+    @cart.destroy
+    session[:cart_id] = nil
+    redirect_to root_path
+  end
+
+  private 
+  def intialize_Cart_Object
+    @cart = @current_cart
   end
 end
