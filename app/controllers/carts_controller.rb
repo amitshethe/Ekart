d792c59e1d643c761553d709d3b9a0d367332e1d
amit <ashethe@deqode.com>
2021-12-20 18:22:16 +0530

    return keyword removed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,7 +6,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
     redirect_to root_path
   end
-
+    
   private 
   def intialize_Cart_Object
     @cart = @current_cart
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,6 +8,5 @@ class Cart < ApplicationRecord
       self.line_items.each do |line_item|
         sum+= line_item.total_price
       end
-      return sum
     end
 end
