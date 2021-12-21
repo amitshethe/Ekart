f7fc00007552fc87f91a7e1bcc5fa2329d9a5227
amit <ashethe@deqode.com>
2021-12-21 17:30:03 +0530

    Line Item changes

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ApplicationRecord
-    belongs_to :product
-    belongs_to :cart
-    belongs_to :order,optional: true
+  belongs_to :product
+  belongs_to :cart
+  belongs_to :order,optional: true
+  
+  def total_price
+    self.quantity ||= 0
+    self.quantity * self.product.price
+  end
 end
