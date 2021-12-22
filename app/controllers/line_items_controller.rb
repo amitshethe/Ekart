b317eaeb713444653c760e439f3f0278df200ec6
amit <ashethe@deqode.com>
2021-12-22 17:09:30 +0530

    Made changes according to last comments i.e 1. Used update_attribute 2. Used exception handling for save

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,52 +1,59 @@
-class LineItemsController < ApplicationController
-  before_action :find_item, only: %i[ destroy add_quantity reduce_quantity]
+# frozen_string_literal: true
 
-  def create       
+class LineItemsController < ApplicationController
+  before_action :find_item, only: %i[destroy add_quantity reduce_quantity]
+
+  def create
     chosen_product = Product.find(params[:product_id])
     current_cart = @current_cart
     if current_cart.products.include?(chosen_product)
-      @line_item = current_cart.line_items.find_by(:product_id => chosen_product)
+      @line_item = current_cart.line_items.find_by(product_id: chosen_product)
       @line_item.quantity += 1
     else
       @line_item = LineItem.new
       @line_item.cart = current_cart
       @line_item.product = chosen_product
-      @line_item.quantity = 1      
+      @line_item.quantity = 1
     end
-    @line_item.save!
-    redirect_to cart_path(current_cart)
+    begin
+      @line_item.save!
+      redirect_to cart_path(current_cart)
+    rescue Exception => e
+      redirect_to request.referrer, alert: e.message
+    end
   end
-    
+
   def destroy
     @line_item.destroy
     redirect_to cart_path(@current_cart)
-  end  
-    
+  end
+
   def add_quantity
     @line_item.quantity ||= 0
     @line_item.quantity += 1
-    @line_item.save!
-    redirect_to cart_path(@current_cart)
-  end
-      
-  def reduce_quantity
-    if @line_item.quantity > 1
-      @line_item.quantity -= 1
+    if @line_item.update_attribute(:quantity, @line_item.quantity)
+      redirect_to cart_path(@current_cart)
+    else
+      redirect_to request.referrer, alert: "Quantity did not add"
     end
-    @line_item.save
-    redirect_to cart_path(@current_cart)
-  end     
-      
+  end
+
+  def reduce_quantity
+    @line_item.quantity -= 1 if @line_item.quantity > 1
+    if @line_item.update_attribute(:quantity, @line_item.quantity)
+      redirect_to cart_path(@current_cart)
+    else
+      redirect_to request.referrer, alert: "Quantity did not reduced"
+    end
+  end
+
   private
+
   def line_item_params
-    params.require(:line_item).permit(:quantity,:product_id, :cart_id)
+    params.require(:line_item).permit(:quantity, :product_id, :cart_id)
   end
 
   def find_item
     @line_item = LineItem.find(params[:id])
-  end      
-    
+  end
 end
-    
-    
-    
