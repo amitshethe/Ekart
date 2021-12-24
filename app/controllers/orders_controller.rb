8bd8c26edd4faed467c7bbf3db337491f813c5a7
amit <ashethe@deqode.com>
2021-12-24 11:54:28 +0530

    Order module update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,36 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
-  def index; end
+  def index
+    @orders = Order.all
+  end
 
-  def show; end
+  def show
+    @order = Order.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @current_cart.line_items.each do |item|
+      @order.line_items << item
+      item.cart_id = nil
+    end
+    if @order.save
+      Cart.destroy(session[:cart_id])
+      session[:cart_id] = nil
+      redirect_to orders_path
+    else
+      redirect_to request.referrer, alert: "order did not place"
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :email, :address, :pay_method)
+  end
 end
